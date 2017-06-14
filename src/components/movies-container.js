c058cd8bd50db34bf54d648ed37ef45cba8bce7b
skim6281 <skim6281@gmail.com>
2017-06-14 14:06:41 -0400

    Show list of movie titles

--- src/components/movies-container.js
+++ src/components/movies-container.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+function renderMovies(movies) {
+  window.movies = movies;
+  return movies.map((movie, ind) => {
+    return(
+      <li key={ind}>
+        {movie.title}
+      </li>
+    )
+  });
+}
+
 export function MoviesContainer(props) {
-  return (<h1>TODO :)</h1>)
+  return (
+    <div>
+      <h1>TODO :)</h1>
+      <ul>
+        {renderMovies(props.movies)}
+      </ul>
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
